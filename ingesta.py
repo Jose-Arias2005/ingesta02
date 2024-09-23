dbdcf197296f2d9392940fa6888d95fb7ab8da6e
José Armando Arias Romero <166131122+Jose-Arias2005@users.noreply.github.com>
2024-09-22 23:10:30 -0500

    ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,20 +1,22 @@
 import mysql.connector
-import csv 
+import csv
+import boto3
 
 mydb = mysql.connector.connect(
-  host="3.89.235.4",
-  port=8005,
-  user="root",
-  password="utec",
-  database="tienda"
+    host="3.89.235.4",
+    port=8005,
+    user="root",
+    password="Utec",
+    database="tienda"
 )
-print(mydb)
-dbQuery='SELECT * FROM fabricantes;'
 
+print(mydb)
+dbQuery = 'SELECT * FROM fabricantes;'
+filename = "fabricantes.csv"
 cursor = mydb.cursor()
 cursor.execute(dbQuery)
 rows = cursor.fetchall()
-fp = open('fabricantes.csv', 'w')
+fp = open(filename, 'w')
 myFile = csv.writer(fp)
 myFile.writerows(rows)
 fp.close()
